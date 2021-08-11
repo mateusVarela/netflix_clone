76992ca75c6f390364c11d926efa8e09509e2c63
Mateus Varela <mvarela038@gmail.com>
2021-08-10 23:23:31 -0300

    added new caroulsel

--- src/components/movieList/movieRow.js
+++ src/components/movieList/movieRow.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import './movieRow.css'
 
 export default ({title, items}) => {
@@ -23,26 +25,28 @@ export default ({title, items}) => {
       setScrollX(scrollValue)
     }
 
+    const posts = items.results.map((item, key) => (`https://image.tmdb.org/t/p/w300${item.poster_path}`))
+
+    const card = items.results.map((item, key) => (
+      <div key={key} className="movieRow--item">
+        <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
+      </div>
+    ))
+
+      console.log(card);
+
+    console.log(posts)
   return (
     <div className="movieRow">
       <h2>
         {title}
       </h2>
-      <div className="movieRow--left" onClick={handLeftArrow}>
-        <NavigateBeforeIcon style={{fontSize: 50}} />
-      </div>
-      <div className="movieRow--right" onClick={handRightArrow}>
-        <NavigateNextIcon style={{fontSize: 50}} />
-      </div>
       <div className="movieList--listArea">
-        <div className="movieRow--list" style={{marginLeft: scrollX, width: items.results.length * 205}}>
-          {items.results.length > 0 && items.results.map((item, key) => (
-            <div key={key} className="movieRow--item">
-              <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
-            </div>
-          ))}
+        <div className="movieRow--list">
+        <AliceCarousel infinite keyboardNavigation mouseTracking items={card}/>
         </div>
       </div>
     </div>
   )
 }
+//<AliceCarousel mo useTracking items={items.results.map((item, key) => (`https://image.tmdb.org/t/p/w300${item.poster_path}`))} />
